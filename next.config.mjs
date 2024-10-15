197e211004d56bdcc4a49d7f02d628027f2e12a8
sky <sodlfmf@gmail.com>
2024-10-15 11:37:47 +0900

    modify config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const prefix = process.env.NODE_ENV === 'production' ? 'https://sodlfmf.github.io/gitServerpage/' : ''
+
 const nextConfig = {
+  output:'export',
+  assetPrefix: prefix,  
   images: {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; img-src 'self' data: https:;",
